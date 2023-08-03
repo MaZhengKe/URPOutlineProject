e25998ce0c46bc3d8666d360fd9b6957c5cf7ae0
MK <ma_zhengke@sina.com>
2023-08-03 11:17:48 +0800

    disable in preview

--- Assets/Outline/Runtime/Scripts/OutlineFeature.cs
+++ Assets/Outline/Runtime/Scripts/OutlineFeature.cs
@@ -54,6 +54,8 @@ namespace KuanMi
         // This method is called when setting up the renderer once per-camera.
         public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
         {
+            if(renderingData.cameraData.camera.name == "Preview Scene Camera")
+                return;
             if (!GetMaterial())
             {
                 Debug.LogErrorFormat(
