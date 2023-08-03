b2d1e78fa6dcc3bf0b627dc7576f4943ccacae57
MK <ma_zhengke@sina.com>
2023-08-03 18:16:18 +0800

    name

--- Assets/BLur/Runtime/Scripts/Volume/BokehBlur.cs
+++ Assets/BLur/Runtime/Scripts/Volume/BokehBlur.cs
@@ -2,7 +2,7 @@
 
 namespace KuanMi.Blur.Runtime
 {
-    [VolumeComponentMenu("KuanMi/Blur/KawaseBlur")]
+    [VolumeComponentMenu("KuanMi/Blur/BokehBlur")]
     public class BokehBlur : BaseBlur
     {
         public ClampedIntParameter Iteration2 = new ClampedIntParameter(32, 16, 128);
